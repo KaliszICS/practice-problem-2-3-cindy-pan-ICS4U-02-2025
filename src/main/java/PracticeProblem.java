a205e7f467d5e578d9e50134d4fa516cdfbd6aa2
cindy pan <cindy.pan@student.tdsb.on.ca>
2025-03-31 02:58:33 +0000

    update

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,16 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
-
-	}
-
-	public static void q1() {
-		//Write question 1 code here
-	}
-
-	public static void q2() {
-		//Write question 2 code here
-	}
-
-	public static void q3() {
-		//Write question 3 code here
-	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
+	public static void bubbleSortString(String[] strings) {
+		int len = strings.length;
+		for (int num = 0; num<len-1; num++){
+			for(int at=0; at<len-num-1; at++){
+				if (strings[at].compareToIgnoreCase(strings[at+1])>0){
+					String temp = strings[at];
+					strings[at]= strings[at+1];
+					strings[at+1]=temp;
+				}
+			}
+		}
 	}
 
 }
